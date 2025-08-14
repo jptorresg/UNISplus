b9f82bbc2eaa6653f54a31d8cca57fa0090ccb45
zapatoazul <137974567+zapatoazul@users.noreply.github.com>
2025-08-14 13:48:00 -0600

    se modificaron las variables de entorno para la base de datos

--- config/db.js
+++ config/db.js
@@ -4,12 +4,11 @@ import pg from "pg";
 import dotenv from 'dotenv';
 dotenv.config();
 
+const connectionString = process.env.DATABASE_URL;
+
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+  connectionString,
+  ssl: { rejectUnauthorized: false } // Render necesita SSL
 });
 
 export default db;
